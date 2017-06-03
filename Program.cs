f4bbf8fd8c18eb33b46138b4d493f6806de49e42
Judit <tolnai.judit12@gmail.com>
2017-06-03 20:06:36 +0200

    Person class

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CreateClassAAffonya
+{
+    class Person
+    {
+        private String name;
+        private int birthDate;
+
+        public Person(String name, int birthDate)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+
+        enum Gender
+        {
+            Female,
+            Male
+        }
+
+        public override string ToString()
+        {
+            return String.Format("This instance of my object has the following: name = {0}, birthDate = {1}", name, birthDate);
+        }
+
+    }
+}
